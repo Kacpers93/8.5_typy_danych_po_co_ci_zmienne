f49948e0a2ed8795f8bb8e7473c0444a75f8423a
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-08 01:47:24 +0100

    Add two folders with triangle area

---
+++ triangleArea1/js/scripts.js
@@ -0,0 +1,9 @@
+var triangleArea;
+var a = prompt('Type triangle\'s base:');
+var h = prompt('Type triangle\'s height:');
+
+triangleArea = a*h/2;
+
+alert('triangle area is:' + triangleArea);
+
+console.log('Base of triangle: ' + a + 'Height of triangle: ' + h + 'Area of a triangle is equal to: ' + triangleArea);
