e1cd3b0ff4829b71ccc7559ebf7df313e6a07ed7
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-08 02:45:41 +0100

    Change content

--- triangleArea2/js/scripts.js
+++ triangleArea2/js/scripts.js
@@ -1,22 +1,10 @@
-//var triangleArea;
-//var a = prompt('Type triangle\'s base:');
-//var h = prompt('Type triangle\'s height:');
-//
-//triangleArea = a*h/2;
-//
-//alert('triangle area is:' + triangleArea);
-//
-//console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-
-
-
 var triangleArea = function() {
     var base = parseFloat(document.getElementById('base').value);
     var height = parseFloat(document.getElementById('height').value);
     var output = document.getElementById('output');
     
     if(isNaN(base) || isNaN(height)) {
-        output.textContent = "You did not enter numbers.";
+        output.textContent = "You did 2not enter numbers.";
     } else {
         var area = 2 / base * height;
         output.textContent = "Area = " + area;
